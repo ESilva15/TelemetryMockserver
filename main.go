d5b26abb6e6ffd9a251fa1bfc4d05cb3c0015e10
Eduardo Silva <eduardosilva@disroot.org>
2026-09-14 18:02:02 +0100

    This was required for the previous commit

--- main.go
+++ main.go
@@ -1,6 +1,32 @@
 package main
 
-import "github.com/ESilva15/TelemetryMockserver/cmd"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/ESilva15/TelemetryMockserver/cmd"
+)
+
+func init() {
+	_ = setupLogger()
+}
+
+func setupLogger() error {
+	output, err := os.OpenFile("./output.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o755)
+	if err != nil {
+		return err
+	}
+
+	logger := slog.New(
+		slog.NewTextHandler(output, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}),
+	)
+
+	slog.SetDefault(logger)
+
+	return nil
+}
 
 func main() {
 	cmd.Execute()
